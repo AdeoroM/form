8e52c46f1ce4ca4cfb81fd73626208f203e4df4c
AdeoroM <adeoro@wawand.co>
2018-11-19 17:26:08 -0500

    Third commit

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-type registry struct {
-	FullName, Email, Pass, PassConf string
+import (
+	"regexp"
+)
+
+type Registry struct {
+	FullName string
+	Email    string
+	Pass     string
+	PassConf string
+	Errors   map[string]string
+}
+
+func ValidateEmail(registry *Registry) bool {
+	registry.Errors = make(map[string]string)
+	re := regexp.MustCompile(".+@-+\\..+")
+	matched := re.Match([]byte(registry.Email))
+	if matched == false {
+		registry.Errors["Email"] = "Please enter a valid email address"
+	}
+	return len(registry.Errors) == 0
+
 }
 
 func main() {
